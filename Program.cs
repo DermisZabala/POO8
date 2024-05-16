c036da441b66bdf2dc632654fe3d9694c8b1743b
DermisZabala <dermiszabala@gmail.com>
2024-05-16 17:48:45 -0400

    POO Clase 8-Clases anonimas

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace POO8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creación de una clase anónima
+            var persona = new {nombre = "Juan", edad = 20};
+            Console.WriteLine($"{persona.nombre} {persona.edad}");
+
+            var otraPersona = new {nombre = "Hamel", edad = 19};
+            Console.WriteLine($"{otraPersona.nombre} {otraPersona.edad}");
+
+            Console.ReadKey();
+        }
+    }
+}
